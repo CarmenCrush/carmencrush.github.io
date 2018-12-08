42ef1a6af6dfaa7f67ef15177b7f3ef3da2aae2b
CarmenCrush <45321041+CarmenCrush@users.noreply.github.com>
2018-12-08 16:16:58 -0500

    Change File Locations (Photos), updated scripts.js

---
+++ gallery/scripts.js
@@ -0,0 +1,14 @@
+function showPic(whichpic) {
+var source = whichpic.getAttribute("href");
+var placeholder = document.getElementByID("placeholder");
+placeholder.setAttribute("src", source);
+}
+var text = whichpic.getAttribute("title");
+var body_element = document.getElementsByTagName("body")[0];
+body_element.childNodes.length;
+
+function countBodyChildren() {
+var body_element = document.getElementsByTagName("body")[0];
+alert (body_element.childNodes.length);
+}
+window.onload = countBodyChildren;
