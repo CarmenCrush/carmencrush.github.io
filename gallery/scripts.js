9fd539a9ae4b4a04de57cc308aa6e351feb13413
CarmenCrush <45321041+CarmenCrush@users.noreply.github.com>
2018-12-17 23:38:37 -0500

    onclick added to new file, showPic.js

--- gallery/scripts.js
+++ gallery/scripts.js
@@ -12,3 +12,5 @@ var body_element = document.getElementsByTagName("body")[0];
 alert (body_element.childNodes.length);
 }
 window.onload = countBodyChildren;
+
+
---
+++ gallery/showPic.js
@@ -0,0 +1,1 @@
+onclick = "showPic(this);"
