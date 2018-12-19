df0db3d263a6dba22a311970f0a289e7d62414a9
CarmenCrush <45321041+CarmenCrush@users.noreply.github.com>
2018-12-19 02:51:12 -0500

    removed text in file

--- gallery/scripts.js
+++ gallery/scripts.js
@@ -12,5 +12,15 @@ var body_element = document.getElementsByTagName("body")[0];
 alert (body_element.childNodes.length);
 }
 window.onload = countBodyChildren;
+onclick = "showPic(this);"
+
+function showPic(whichpic) {
+	var source = whichpic.getAttribute("href");
+	var placeholder = document.getElementByID("placeholder");
+	placeholder.setAttribute("src", source);
+	var text = whichpic.getAttribute("title");
+	var description = document.getElementByID("description");
+	description.firstChild.nodeValue = text;
+}
 
 
