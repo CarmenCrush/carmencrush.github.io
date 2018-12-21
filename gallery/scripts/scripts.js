a2388d82e494c1b55a587e10feb2d6da15eaedf0
CarmenCrush <45321041+CarmenCrush@users.noreply.github.com>
2018-12-21 16:15:52 -0500

    changed from just scripts.js to showPic.js + scripts.js

--- gallery/scripts/scripts.js
+++ gallery/scripts/scripts.js
@@ -14,7 +14,7 @@ var body_element = document.getElementsByTagName("body")[0];
 window.onload = countBodyChildren;
 //onclick = "showPic(this);"
 
-function showPic(whichpic) {
+/*function showPic(whichpic) {
 	var source = whichpic.getAttribute("href");
 	var placeholder = document.getElementById("placeholder");
 	placeholder.setAttribute("src", source);
@@ -24,4 +24,4 @@ function showPic(whichpic) {
 	description.firstChild.nodeValue = text;
 	//alert(description.firstChild.nodeValue);
 }
-
+*/
