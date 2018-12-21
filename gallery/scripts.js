40e5ebb9eca77df0a3077e7ff53c10b111789300
CarmenCrush <45321041+CarmenCrush@users.noreply.github.com>
2018-12-21 15:19:33 -0500

    notes

--- gallery/scripts.js
+++ gallery/scripts.js
@@ -1,15 +1,16 @@
-function showPic(whichpic) {
+/*function showPic(whichpic) {
 var source = whichpic.getAttribute("href");
 var placeholder = document.getElementByID("placeholder");
 placeholder.setAttribute("src", source);
 }
 var text = whichpic.getAttribute("title");
 var body_element = document.getElementsByTagName("body")[0];
-body_element.childNodes.length;
+body_element.childNodes.length; */
 
 function countBodyChildren() {
 var body_element = document.getElementsByTagName("body")[0];
-alert (body_element.childNodes.length);
+//alert (body_element.childNodes.length);
+//i got 20
 }
 window.onload = countBodyChildren;
 onclick = "showPic(this);"
@@ -22,5 +23,7 @@ function showPic(whichpic) {
 	var description = document.getElementByID("description");
 	description.firstChild.nodeValue = text;
 }
+alert(description.firstChild.nodeValue);
+//this alert did nothing
 
-
+//why do i need to put this in another .js file?
