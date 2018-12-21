d392ca93126355db87bb66eca5e523579289e0c8
CarmenCrush <45321041+CarmenCrush@users.noreply.github.com>
2018-12-21 15:59:41 -0500

    Working Basic Gallery
    Got functionality working for gallery folder.

--- gallery/scripts/scripts.js
+++ gallery/scripts/scripts.js
@@ -10,20 +10,18 @@ body_element.childNodes.length; */
 function countBodyChildren() {
 var body_element = document.getElementsByTagName("body")[0];
 //alert (body_element.childNodes.length);
-//i got 20
 }
 window.onload = countBodyChildren;
-onclick = "showPic(this);"
+//onclick = "showPic(this);"
 
 function showPic(whichpic) {
 	var source = whichpic.getAttribute("href");
-	var placeholder = document.getElementByID("placeholder");
+	var placeholder = document.getElementById("placeholder");
 	placeholder.setAttribute("src", source);
 	var text = whichpic.getAttribute("title");
-	var description = document.getElementByID("description");
+	var description = document.getElementById("description");
+	console.log("description", description);
 	description.firstChild.nodeValue = text;
+	//alert(description.firstChild.nodeValue);
 }
-alert(description.firstChild.nodeValue);
-//this alert did nothing
 
-//why do i need to put this in another .js file?
