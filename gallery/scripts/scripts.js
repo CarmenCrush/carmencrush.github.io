7761392c8138a7477e59d1ba63b0b714985b65de
CarmenCrush <45321041+CarmenCrush@users.noreply.github.com>
2018-12-29 23:27:26 -0500

    moved JS onclick to scripts folder, removed showPic.js functionality & added to scripts.js

--- gallery/scripts/scripts.js
+++ gallery/scripts/scripts.js
@@ -14,7 +14,7 @@ var body_element = document.getElementsByTagName("body")[0];
 window.onload = countBodyChildren;
 //onclick = "showPic(this);"
 
-/*function showPic(whichpic) {
+function showPic(whichpic) {
 	var source = whichpic.getAttribute("href");
 	var placeholder = document.getElementById("placeholder");
 	placeholder.setAttribute("src", source);
@@ -24,4 +24,30 @@ window.onload = countBodyChildren;
 	description.firstChild.nodeValue = text;
 	//alert(description.firstChild.nodeValue);
 }
-*/
+
+function prepareGallery() {
+	if (!document.getElementsByTagName || !document.getElementById) return false;
+	if (!document.getElementById("imagegallery")) return false;
+	var gallery = document.getElementById("imagegallery");
+	var links = gallery.getElementsByTagName("a");
+	for ( var i=0; i < links.length; i++) {
+		links[i].onclick = function() {
+			showPic(this); 
+			return false;
+		}
+	}
+}
+
+function addLoadEvent(func) {
+	var oldonload = window.onload;
+	if (typeof window.onload != 'function') {
+		window.onload = func;
+	} else {
+		window.onload = function () {
+			oldonload();
+			func();
+		}
+	}
+}
+addLoadEvent(prepareGallery);
+
--- gallery/scripts/showPic.js
+++ gallery/scripts/showPic.js
@@ -1,11 +1,1 @@
-function showPic(whichpic) {
-	var source = whichpic.getAttribute("href");
-	var placeholder = document.getElementById("placeholder");
-	placeholder.setAttribute("src", source);
-	var text = whichpic.getAttribute("title");
-	var description = document.getElementById("description");
-	console.log("description", description);
-	description.firstChild.nodeValue = text;
-	//alert(description.firstChild.nodeValue);
-}
 
