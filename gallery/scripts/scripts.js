b857bed9ffbcab15f618630ba6be7a985a6257c0
CarmenCrush <45321041+CarmenCrush@users.noreply.github.com>
2019-01-03 04:10:44 -0500

    added mmore elegant degredation

--- gallery/scripts/scripts.js
+++ gallery/scripts/scripts.js
@@ -15,14 +15,18 @@ window.onload = countBodyChildren;
 //onclick = "showPic(this);"
 
 function showPic(whichpic) {
+	if (!document.getElementById("placeholder")) return false;
 	var source = whichpic.getAttribute("href");
 	var placeholder = document.getElementById("placeholder");
 	placeholder.setAttribute("src", source);
-	var text = whichpic.getAttribute("title");
-	var description = document.getElementById("description");
-	console.log("description", description);
-	description.firstChild.nodeValue = text;
-	//alert(description.firstChild.nodeValue);
+	if (document.getElementById("description")) {
+		//var text = whichpic.getAttribute("title");
+		var text = whichpic.getAttribute("title") ? whichpic.getAttribute("title");
+		var description = document.getElementById("description");
+		//console.log("description", description);
+		description.firstChild.nodeValue = text;
+	}
+	return true;
 }
 
 function prepareGallery() {
@@ -32,8 +36,7 @@ function prepareGallery() {
 	var links = gallery.getElementsByTagName("a");
 	for ( var i=0; i < links.length; i++) {
 		links[i].onclick = function() {
-			showPic(this); 
-			return false;
+		 return !showPic(this); 
 		}
 	}
 }
