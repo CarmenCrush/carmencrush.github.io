92d58943ae2ee84d5461d39f046a1f12d67dc2b6
CarmenCrush <45321041+CarmenCrush@users.noreply.github.com>
2019-01-26 23:02:28 -0500

    preparePlaceholder function replacing HTML, rearrange functions to match book

--- example.js
+++ example.js
@@ -16,8 +16,19 @@ function insertParagraph(text) {
 	info+= para.nodeType;
 	alert(info);*/
 }
-var para = document.createElement("p");
-var testdiv = document.getElementById("testdiv");
-testdiv.appendChild(para);
-var txt = document.createTextNode("Hello world");
 
+window.onload = function() {
+  var para = document.createElement("p");
+  var txt = document.createTextNode("Hello world");
+  para.appendChild(txt);
+  var testdiv = document.getElementById("testdiv");
+  testdiv.appendChild(para);  
+}
+
+/*window.onload = function() {
+  var para = document.createElement("p");
+  var testdiv = document.getElementById("testdiv");
+  testdiv.appendChild(para);
+  var txt = document.createTextNode("Hello world");
+  para.appendChild(txt);
+}*/
--- gallery/scripts/scripts.js
+++ gallery/scripts/scripts.js
@@ -6,6 +6,45 @@ placeholder.setAttribute("src", source);
 var text = whichpic.getAttribute("title");
 var body_element = document.getElementsByTagName("body")[0];
 body_element.childNodes.length; */
+//functionality of adding multiple js scripts to load after HTML loads - outdated technique
+function addLoadEvent(func) {
+	var oldonload = window.onload;
+	if (typeof window.onload != 'function') {
+		window.onload = func;
+	} else {
+		window.onload = function () {
+			oldonload();
+			func();
+		}
+	}
+}
+//creating an insertAfter function
+function insertAfter(newElement, targetElement) {
+	var parent = targetElement.parentNode;
+	if (parent.lastChild == targetElement) {
+		parent.appendChild(newElement);
+	} else {
+		parent.insertBefore(newElement, targetElement.nextSibling);
+	}
+}
+function preparePlaceholder() {
+	if (!document.createElement) return false;
+	if (!document.createTextNode) return false;
+	if (!document.getElementById) return false;
+	if (!document.getElementById("imagegallery")) return false;
+	var placeholder = document.createElement("img");
+	placeholder.setAttribute("id", "placeholder");
+	placeholder.setAttribute("src", "images/placeholder.gif");
+	placeholder.setAttribute("alt", "my image gallery");
+	var description = document.createElement("p");
+	description.setAttribute("id", "description");
+	var desctext = doctype.createTextNode("Choose an image");
+	description.appendChild(desctext);
+	//using insertAfter in preparePlaceholder
+	var gallery = document.getElementByID("imagegallery");
+	insertAfter(placeholder, gallery);
+	insertAfter(description, placeholder);
+}
 
 function countBodyChildren() {
 var body_element = document.getElementsByTagName("body")[0];
@@ -14,8 +53,21 @@ var body_element = document.getElementsByTagName("body")[0];
 window.onload = countBodyChildren;
 //onclick = "showPic(this);"
 
+function prepareGallery() {
+	if (!document.getElementsByTagName || !document.getElementById) return false;
+	if (!document.getElementById("imagegallery")) return false;
+	var gallery = document.getElementById("imagegallery");
+	var links = gallery.getElementsByTagName("a");
+	for ( var i=0; i < links.length; i++) {
+		links[i].onclick = function() {
+		 return showPic(this) ? false : true;
+		}
+		links[i].onkeypress = links[i].onclick;
+	}
+}
+
 function showPic(whichpic) {
-	if (!document.getElementById("placeholder")) return false;
+	if (!document.getElementById("placeholder")) return true;
 	var source = whichpic.getAttribute("href");
 	var placeholder = document.getElementById("placeholder");
 	if (placeholder.nodeName != "IMG") return false;
@@ -31,28 +83,11 @@ function showPic(whichpic) {
 	return true;
 }
 
-function prepareGallery() {
-	if (!document.getElementsByTagName || !document.getElementById) return false;
-	if (!document.getElementById("imagegallery")) return false;
-	var gallery = document.getElementById("imagegallery");
-	var links = gallery.getElementsByTagName("a");
-	for ( var i=0; i < links.length; i++) {
-		links[i].onclick = function() {
-		 return showPic(this) ? false : true;
-		}
-	}
-}
 
-function addLoadEvent(func) {
-	var oldonload = window.onload;
-	if (typeof window.onload != 'function') {
-		window.onload = func;
-	} else {
-		window.onload = function () {
-			oldonload();
-			func();
-		}
-	}
-}
 addLoadEvent(prepareGallery);
+//moving placeholder showPic script to js from html
+
+
+
+
 
